7cebb35034e8d8f3f93d7ce573d6ef9cf7366791
Wxw <weiwei_2024024@qq.com>
2025-06-01 19:20:18 +0800

    modify code

--- 练习/Chapt4/mk.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-del chinaz.com.txt /s
-del 说明.htm /s
-del 西西软件园.txt /s
-del 西西软件.url /s
-::copy d:\jb51tools\jb_down\soft\ !cd!
-mkdir jb51.net
-del %0 | move *.* jb51.net
